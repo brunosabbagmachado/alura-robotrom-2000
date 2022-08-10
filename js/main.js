63ee51286d093fefad2daf70b415992cafe3f45d
Bruno S. Machado <bruno.sabbagmachado@outlook.com>
2022-08-09 22:01:48 -0300

    modulo 3 do curso

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
-const robotron = document.querySelector('#robotron');
+const subtrair = document.querySelector('#subtrair');
+const somar = document.querySelector('#somar');
+const braco = document.querySelector('#braco');
 
-robotron.addEventListener("click", (evento) => {
-    console.log('Cliquei no robô.');
-    console.log(evento);
+const controles = document.querySelectorAll('.controle-ajuste');
+
+controles.forEach ( (elemento) => {
+    elemento.addEventListener('click', (evento) => {
+        console.log(evento)
+    })
 })
 
-function dizOi (nome) {
-    console.log('Oi ' + nome);
-    console.log("Bem-vindo ao Robotron 2000");
-}
+somar.addEventListener('click', () => {manipulaDados('somar')})
 
-dizOi('Bruno');
+subtrair.addEventListener('click', () => {manipulaDados('subtrair')})
+
+function manipulaDados(operacao) {
+    if(operacao === "subtrair"){
+        braco.value = parseInt(braco.value) - 1;
+    } else {
+        braco.value = parseInt(braco.value) + 1;
+    }
+}
